cb115293835a2ae0f8f6a5008bcb42327c545edf
Rogier Stegeman <rogier.stegeman@vartion.com>
2020-05-19 09:27:12 +0200

    logger env trueing

--- scripts/python/env.py
+++ scripts/python/env.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+trues = (True, "True", "true")
+
 # Logging
 LOG_LVL = os.getenv("LOG_LVL", "INFO")
-COL_LOGS = os.getenv("COL_LOGS") == "true"
+COL_LOGS = os.getenv("COL_LOGS", "true") in trues
